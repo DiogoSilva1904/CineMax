69c63316fe769779ede5030f6cd9f55103fedee8
Rafa548 <rafael3vilaca30@gmail.com>
2024-05-14 20:28:01 +0100

    navbar fix

--- frontend/cinemax/src/app/navbar/navbar.component.ts
+++ frontend/cinemax/src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ MatSidenavModule ],
+  imports: [ MatSidenavModule,MatListModule,MatIconModule, ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
