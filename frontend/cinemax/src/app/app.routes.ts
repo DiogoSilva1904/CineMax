277ef138eaa4e64f109d5b9103e654531d6fb819
miguel <miguelcruzeiro70@gmail.com>
2024-05-14 19:38:57 +0100

    add-movie

--- frontend/cinemax/src/app/app.routes.ts
+++ frontend/cinemax/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 export const routes: Routes = [
-    { path: '', component: MoviesComponent }
+    { path: '', component: MoviesComponent },
+    { path: 'addmovie', component: AddMovieComponent }
 ];
--- frontend/cinemax/src/app/movies/movies.component.ts
+++ frontend/cinemax/src/app/movies/movies.component.ts
@@ -2,6 +2,7 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -18,5 +19,16 @@ export class MoviesComponent {
     { title: 'The Godfather: Part II', year: 1974 },
     { title: 'The Dark Knight', year: 2008 }
   ];
+
+  constructor(private router: Router) {
+  }
+
+  goToAddMovie() {
+    console.log('goToAddMovie');
+    this.router.navigate(['/addmovie'])
+    .then(() => {
+      window.location.reload();
+    });
+  }
 
 }
