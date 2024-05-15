48bc9f75cddc6d400fc176f915c3b12582c707f8
TK <kauati.toiti@gmail.com>
2024-05-15 19:22:53 +0100

    session and room service

--- frontend/cinemax/src/service/api.service.ts
+++ frontend/cinemax/src/service/api.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Movie} from "../interface/movie";
+import {Session} from "node:inspector";
+import {Room} from "../interface/room";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private host = "localhost/8080";
-  private moviesUrl = '/api/movies';
+  private host = "http://localhost/8080";
+  private moviesUrl = `${this.host}/api/movies`;
+  private sessionsUrl = `${this.host}/api/sessions`;
+  private roomsUrl = `${this.host}/api/rooms`;
 
   constructor(private http: HttpClient) { }
+
+  // =============== Movies api methods ===============
+
 
   getAllMovies(): Observable<Movie[]> {
     return this.http.get<Movie[]>(`${this.moviesUrl}`);
@@ -23,4 +30,33 @@ export class ApiService {
   saveMovie(movie: Movie): Observable<Movie> {
     return this.http.post<Movie>(`${this.moviesUrl}`, movie);
   }
+
+  // =============== Session api methods ===============
+
+  getAllSessions(): Observable<Session[]> {
+    return this.http.get<Session[]>(`${this.sessionsUrl}`);
+  }
+
+  getSessionById(id: number): Observable<Session> {
+    return this.http.get<Session>(`${this.sessionsUrl}/${id}`);
+  }
+
+  saveSession(session: Session): Observable<Session> {
+    return this.http.post<Session>(`${this.sessionsUrl}`, session);
+  }
+
+  // =============== Room api methods ===============
+
+  getAllRooms(): Observable<Room[]> {
+    return this.http.get<Room[]>(`${this.roomsUrl}`);
+  }
+
+  getRoomById(id: number): Observable<Room> {
+    return this.http.get<Room>(`${this.roomsUrl}/${id}`);
+  }
+
+  saveRoom(room: Room): Observable<Room> {
+    return this.http.post<Room>(`${this.roomsUrl}`, room);
+  }
+
 }
