ec51c128da1e96d51090e09fb3354ddfa5f1d195
Rafa548 <rafael3vilaca30@gmail.com>
2024-05-15 18:47:15 +0100

    qrcode page added for later use

--- frontend/cinemax/src/app/app.routes.ts
+++ frontend/cinemax/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { AddMovieComponent } from './add-movie/add-movie.component';
+import { QrcodepageComponent } from './qrcodepage/qrcodepage.component';
 
 export const routes: Routes = [
     { path: '', component: FrontpageComponent },
     { path: 'movies', component: MoviesComponent},
-    { path: 'addmovie', component: AddMovieComponent}
+    { path: 'addmovie', component: AddMovieComponent},
+    { path: "qrcode", component: QrcodepageComponent}
 ];
---
+++ frontend/cinemax/src/app/qrcodepage/qrcodepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
+
+@Component({
+  selector: 'app-qrcodepage',
+  standalone: true,
+  imports: [QRCodeModule],
+  templateUrl: './qrcodepage.component.html',
+  styleUrl: './qrcodepage.component.css'
+})
+export class QrcodepageComponent {
+  
+}
