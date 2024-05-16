ef2bd555e31fc754c40dd5741808869674d99dfe
Rafa548 <rafael3vilaca30@gmail.com>
2024-05-16 14:38:46 +0100

    service fix

--- frontend/cinemax/src/app/service/api.service.ts
+++ frontend/cinemax/src/app/service/api.service.ts
@@ -6,8 +6,6 @@ import { Injectable } from '@angular/core';
 export class ApiService {
   baseUrl = "http://localhost:8080/api";
 
-  baseURL = 'http://localhost:8080';
-
   constructor() { }
 
   async getSessions() {
@@ -29,19 +27,19 @@ export class ApiService {
   }
 
   async getMovies() {
-    const url = `${this.baseURL}/api/movies`;
+    const url = `${this.baseUrl}/movies`;
     const response = await fetch(url, { method: 'GET' });
     return await response.json() ?? undefined;
   }
 
   async addMovie(movie: any) {
-    const url = `${this.baseURL}/api/movies`;
+    const url = `${this.baseUrl}/movies`;
     const response = await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(movie) });
     return await response.json() ?? undefined;
   }
 
   async deleteMovie(movieId: string) {
-    const url = `${this.baseURL}/api/movies/${movieId}`;
+    const url = `${this.baseUrl}/movies/${movieId}`;
     console.log('Deleting movie: url:', url);
     const response = await fetch(url, { method: 'DELETE' });
     return await response.statusText ?? undefined;
