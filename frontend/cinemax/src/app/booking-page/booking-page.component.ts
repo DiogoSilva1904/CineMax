2b44ba30e5c5ff98cfede751afcdfffb902ea99d
TK <kauati.toiti@gmail.com>
2024-05-16 19:47:43 +0100

    Style so far

--- frontend/cinemax/src/app/booking-page/booking-page.component.ts
+++ frontend/cinemax/src/app/booking-page/booking-page.component.ts
@@ -76,9 +76,9 @@ export class BookingPageComponent {
     { seatIdentifier: "D10", occupied: false },
   ];
   selectedSeats: string[] = [];
-  sessionTime: string = '18:00'; 
-  day: string = 'Monday'; 
-  movieName: string = 'Avengers: Endgame'; 
+  sessionTime: string = '18:00';
+  day: string = 'Monday';
+  movieName: string = 'Avengers: Endgame';
   totalPrice: number = 0;
   ApiService= inject(ApiService);
   Sessions: Session[] = [];
@@ -108,7 +108,7 @@ export class BookingPageComponent {
     } else {
       this.selectedSeats.push(seatIdentifier);
     }
-    this.calculateTotalPrice(); 
+    this.calculateTotalPrice();
   }
 
   calculateTotalPrice() {
