20d132827d78c9365ec42765a64cc2def8b4256e
Rafa548 <rafael3vilaca30@gmail.com>
2024-06-01 17:01:28 +0100

    images working in all pages

--- frontend/cinemax/src/app/movie-sessions/movie-sessions.component.ts
+++ frontend/cinemax/src/app/movie-sessions/movie-sessions.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
 import {ClientNavbarComponent} from "../client-navbar/client-navbar.component";
 import {NgForOf, NgIf} from "@angular/common";
 import {Router, ActivatedRoute } from '@angular/router';
 import {ApiService} from "../service/api.service";
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -16,22 +17,41 @@ import {ApiService} from "../service/api.service";
   templateUrl: './movie-sessions.component.html',
   styleUrl: './movie-sessions.component.css'
 })
-export class MovieSessionsComponent {
+export class MovieSessionsComponent implements OnInit{
   movie: any;
+  imageUrl: any;
 
   ApiDataService = inject(ApiService);
 
-  constructor(private route: ActivatedRoute,private router: Router) {
+  constructor(private route: ActivatedRoute,private router: Router, private sanitizer: DomSanitizer) {
+    
+  }
+
+  ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const movieId = params.get('id');
       // Fetch movie details using the movie id
       this.ApiDataService.getMovie(movieId).then((movie) => {
         this.movie = movie;
         console.log(this.movie);
+        this.loadImage();
       }).catch(error => {
         console.error('Error fetching movie:', error);
       });
     });
+  }
+
+  async loadImage(): Promise<void> {
+    if (this.movie.imagePath == null) {
+      this.imageUrl = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+    }
+    else {
+      const imageBlob = await this.ApiDataService.getImage(this.movie.imagePath);
+      if (imageBlob) {
+        const objectURL = URL.createObjectURL(imageBlob);
+        this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      }
+    }
   }
 
   goToChooseSeat(sessionId: number): void {
--- frontend/cinemax/src/app/mytickets/mytickets.component.ts
+++ frontend/cinemax/src/app/mytickets/mytickets.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../service/api.service';
 import {NgForOf, NgIf, NgClass } from "@angular/common";
 import { ClientNavbarComponent } from '../client-navbar/client-navbar.component';
 import { QRCodeModule } from 'angularx-qrcode';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mytickets',
@@ -23,16 +24,33 @@ export class MyticketsComponent implements OnInit {
 
   tickets: any = [];
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.loadTickets();
+  }
+
+  async loadImage(ticket: any): Promise<void> {
+    const movie = ticket.session.movie;
+    if (movie.imagePath == null) {
+      ticket.imageUrl = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+    }
+    else {
+      const imageBlob = await this.apiService.getImage(movie.imagePath);
+      if (imageBlob) {
+        const objectURL = URL.createObjectURL(imageBlob);
+        ticket.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      }
+    }
   }
 
   private async loadTickets() {
     try {
       const username = localStorage.getItem('username');
       const ticketData = await this.apiService.getReservationsByUser(username);
+      for (const ticket of ticketData) {
+        this.loadImage(ticket);
+      }
       this.tickets = ticketData;
       console.log(this.tickets);
     } catch (error) {
